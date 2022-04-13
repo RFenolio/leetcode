b08be4fffe380b8f181620ced3ae104d51f958a7
Ron <mordoc@gmail.com>
2022-04-13 10:49:17 -0700

    clean up

--- 139_word_break.py
+++ 139_word_break.py
@@ -5,11 +5,12 @@ class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         wordSet = set(wordDict)
     
+        # have to define a sub function because we can't hash a mutable list for memoization
         @lru_cache
         def wordBreakCached(s) -> bool:
             if not s:
                 return True
-            for idx in range(1, len(s) + 1):
+            for idx in range(len(s) + 1):
                 if s[:idx] in wordSet and wordBreakCached(s[idx:]):
                     return True
             return False
@@ -21,5 +22,5 @@ assert s.wordBreak('leetcode', ['leet', 'code']) == True
 assert s.wordBreak(s = "applepenapple", wordDict = ["apple","pen"]) == True
 assert s.wordBreak(s = "catsandog", wordDict = ["cats","dog","sand","and","cat"]) == False
 long_word = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab"
-aas_dict = ["a","aa","aaa","aaaa","aaaaa","aaaaaa","aaaaaaa","aaaaaaaa","aaaaaaaaa","aaaaaaaaaa"]
-assert s.wordBreak(long_word, aas_dict) == False
+aaaa_dict = ["a","aa","aaa","aaaa","aaaaa","aaaaaa","aaaaaaa","aaaaaaaa","aaaaaaaaa","aaaaaaaaaa"]
+assert s.wordBreak(long_word, aaaa_dict) == False
